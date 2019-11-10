aaa07030aee5695c01a02451acf4ae8b76578ebf
Volnov Petr <41264338+Neafiol@users.noreply.github.com>
2019-11-11 01:22:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,1 @@
-import os
 
-bdname = 'master_bot_kit_db'
-bduser = 'postgres'
-bdpassword = 'nef441'
-bdhost = '51.79.69.179'
-admin_pass="nef441"
-admin_host="http://localhost:3000"
-if 'SERVER' in os.environ:
-    bdhost = '127.0.0.1'
-    admin_host = "http://mbk.neafiol.site"
-
-bdport = 5432
