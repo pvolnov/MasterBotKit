3f54f1942e7e7aa2a6cc33d4bd50079a1511718c
PetrVolnov <neafiol@ya.ru>
2019-11-29 17:37:33 +0300

    mbk

--- config.py
+++ config.py
@@ -4,11 +4,10 @@ bdname = 'master_bot_kit_db'
 bduser = 'postgres'
 bdpassword = 'nef441'
 bdhost = '51.79.69.179'
-ADMIN_PASS = "nef441"
-ADMIN_HOST = "http://localhost:3000"
+admin_pass="nef441"
+admin_host="http://localhost:3000"
 if 'SERVER' in os.environ:
     bdhost = '127.0.0.1'
-    ADMIN_HOST = "http://mbk.neafiol.site"
+    admin_host = "http://mbk.neafiol.site"
 
 bdport = 5432
-SERVER_PORT = 8000
